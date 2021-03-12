f596f347bf69d2335f186c8c8adf029f8cf4eb36
Kevin Enloe <enloe92@gmail.com>
2021-03-12 13:32:51 -0700

    new

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://morning-journey-10956.herokuapp.com/api',
+  API_ENDPOINT: 'https://spaced-repetition-server-kevin.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
